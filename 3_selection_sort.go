b887e702008193d7d77e43e60d8f7723758b5d89
kekelberi <alexei.gaponov@gmail.com>
2023-02-28 21:00:19 -0600

    save

--- 3_selection_sort.go
+++ 3_selection_sort.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
-func selectSort(arr []int) []int {
-	for i := 0; i < len(arr); i++ {
-		minI := i
-		for j := i + 1; j < len(arr); j++ {
-			if arr[j] < arr[minI] {
-				minI = j
-			}
-		}
-		tap := arr[i]
-		arr[i] = arr[minI]
-		arr[minI] = tap
-	}
-	return arr
-}
-
 func main() {
 	arr := []int{0, 5, 6, 7, 2, 3, 2, 6, 8, 22, 55, 44, 77, 1, 2, 77, 88, 2, 7, 9}
-	sortedArr := selectSort(arr)
-	fmt.Println(sortedArr)
+	selectSort(arr)
+	fmt.Print(arr)
+}
+
+func selectSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		indMin := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[indMin] {
+				indMin = j
+			}
+		}
+		tm := arr[i]
+		arr[i] = arr[indMin]
+		arr[indMin] = tm
+	}
+	return arr
 }
